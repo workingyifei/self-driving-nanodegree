1495e8fc6ab218597313cf455831738804b98ae5
Yifei Li <workingyifei@gmail.com>
2018-07-31 22:45:15 -0700

    fixed the SIGSEGV error

--- CarND-Extended-Kalman-Filter-Project/src/FusionEKF.cpp
+++ CarND-Extended-Kalman-Filter-Project/src/FusionEKF.cpp
@@ -21,6 +21,8 @@ FusionEKF::FusionEKF() {
   R_radar_ = MatrixXd(3, 3);
   H_laser_ = MatrixXd(2, 4);
   Hj_ = MatrixXd(3, 4);
+  ekf_.F_ = MatrixXd(4, 4);
+  ekf_.P_ = MatrixXd(4, 4);
 
   //measurement covariance matrix - laser
   R_laser_ << 0.0225, 0,
@@ -31,6 +33,9 @@ FusionEKF::FusionEKF() {
         0, 0.0009, 0,
         0, 0, 0.09;
 
+  H_laser_ << 1, 0, 0, 0,
+          0, 1, 0, 0;
+
   /**
   TODO:
     * Finish initializing the FusionEKF.
@@ -38,14 +43,6 @@ FusionEKF::FusionEKF() {
   */
   noise_ax = 9;
   noise_ay = 9;
-
-  H_laser_ << 1, 0, 0, 0,
-         0, 1, 0, 0;
-
-  Hj_ << 0, 0, 0, 0,
-         0, 0, 0, 0,
-         0, 0, 0, 0;
-
 }
 
 /**
@@ -68,19 +65,19 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
     */
     // first measurement
     cout << "EKF: " << endl;
-    ekf_.x_ = VectorXd(4);
+    ekf_.x_  = VectorXd(4);
 //    ekf_.x_ << 1, 1, 1, 1;
 
     //init the kalman filter
     ekf_.P_ << 1, 0, 0, 0,
-            0, 1, 0, 0,
-            0, 0, 1000, 0,
-            0, 0, 0, 1000;
+               0, 1, 0, 0,
+               0, 0, 1000, 0,
+               0, 0, 0, 1000;
 
     ekf_.F_ << 1, 0, 0, 0,
-            0, 1, 0, 0,
-            0, 0, 1, 0,
-            0, 0, 0, 1;
+               0, 1, 0, 0,
+               0, 0, 1, 0,
+               0, 0, 0, 1;
 
 //    ekf_.Init(ekf_.x_, ekf_.P_,  ekf_.F_, ekf_.H_, ekf_.Q_, ekf_.R_);
 
@@ -89,23 +86,22 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
       Convert radar from polar to cartesian coordinates and initialize state.
       */
 
-      float rho = measurement_pack.raw_measurements_[0];
-      float phi = measurement_pack.raw_measurements_[1];
+      double rho = measurement_pack.raw_measurements_[0];
+      double phi = measurement_pack.raw_measurements_[1];
 //      float rho_dot = measurement_pack.raw_measurements_[2];
 
-      float px = rho * cos(phi);
-      float py = rho * sin(phi);
+      double px = rho * cos(phi);
+      double py = rho * sin(phi);
 
       ekf_.x_ << px, py, 0, 0;
-
     }
     else if (measurement_pack.sensor_type_ == MeasurementPackage::LASER) {
       /**
       Initialize state.
       */
       ekf_.x_ << measurement_pack.raw_measurements_[0], measurement_pack.raw_measurements_[1], 0, 0;
-
     }
+
 
     previous_timestamp_ = measurement_pack.timestamp_;
 
@@ -113,8 +109,6 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
     is_initialized_ = true;
     return;
   }
-
-  cout<<"EKF init done"<<endl;
 
   /*****************************************************************************
    *  Prediction
@@ -128,21 +122,22 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
      * Use noise_ax = 9 and noise_ay = 9 for your Q matrix.
    */
 
-  double dt = (measurement_pack.timestamp_ - previous_timestamp_) / 1000000.0;
+  float dt = (measurement_pack.timestamp_ - previous_timestamp_) / 1000000.0;
   previous_timestamp_ = measurement_pack.timestamp_;
 
-  double dt_2 = dt * dt;
-  double dt_3 = dt_2 * dt;
-  double dt_4 = dt_3 * dt;
+  float dt_2 = dt * dt;
+  float dt_3 = dt_2 * dt;
+  float dt_4 = dt_3 * dt;
 
   ekf_.F_(0, 2) = dt;
   ekf_.F_(1, 3) = dt;
 
   //set the process covariance matrix Q
+  ekf_.Q_ = MatrixXd(4, 4);
   ekf_.Q_ <<  dt_4/4*noise_ax, 0, dt_3/2*noise_ax, 0,
-          0, dt_4/4*noise_ay, 0, dt_3/2*noise_ay,
-          dt_3/2*noise_ax, 0, dt_2*noise_ax, 0,
-          0, dt_3/2*noise_ay, 0, dt_2*noise_ay;
+              0, dt_4/4*noise_ay, 0, dt_3/2*noise_ay,
+              dt_3/2*noise_ax, 0, dt_2*noise_ax, 0,
+              0, dt_3/2*noise_ay, 0, dt_2*noise_ay;
 
 
   ekf_.Predict();
@@ -159,13 +154,14 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
 
   if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
     // Radar updates
+    ekf_.H_ = MatrixXd(3, 4);
     Hj_ = tools.CalculateJacobian(ekf_.x_);
     ekf_.H_ = Hj_;
-
     ekf_.R_ = R_radar_;
     ekf_.UpdateEKF(measurement_pack.raw_measurements_);
   } else {
     // Laser updates
+    ekf_.H_ = MatrixXd(2, 4);
     ekf_.H_ = H_laser_;
     ekf_.R_ = R_laser_;
     ekf_.Update(measurement_pack.raw_measurements_);
