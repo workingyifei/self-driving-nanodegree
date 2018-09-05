9c3e04b0404bc467d5bec32adb4928413e997157
Yifei Li <workingyifei@gmail.com>
2018-09-05 00:21:25 -0700

    added MPC algorithm, not working

--- CarND-MPC-Project/src/main.cpp
+++ CarND-MPC-Project/src/main.cpp
@@ -98,8 +98,55 @@ int main() {
           * Both are in between [-1, 1].
           *
           */
+
+          // Orientation transformation (global to car coordinates)
+          for (int i=0; i<ptsx.size(); i++) {
+
+            // x and y to zero
+            double shift_x = ptsx[i] - px;
+            double shift_y = ptsy[i] - py;
+
+            // psi to zero
+            ptsx[i] = (shift_x * cos(0 - psi) - shift_y * sin(0 - psi));
+            ptsy[i] = (shift_x * sin(0 - psi) + shift_y * cos(0 - psi));
+          }
+
+          double* ptrx = &ptsx[0];
+          Eigen::Map<Eigen::VectorXd> ptsx_transform(ptrx, 6);
+
+          double* ptry = &ptsy[0];
+          Eigen::Map<Eigen::VectorXd> ptsy_transform(ptry, 6);
+
+          auto coeffs = polyfit(ptsx_transform, ptsy_transform, 3);
+
+          // Calculate cte and epsi
+          double cte  = polyeval(coeffs, 0);  // Simplified estimation
+          double epsi = -atan(coeffs[1]);     // Simplified due to psi, px and py is zero
+
+
+          // calculate x position according to delay (y position not affected due to vehicle coordinates)
+          const double delay_s = .1;          // delay in seconds
+          double x_delay = 0.0 + v * delay_s;
+
+
+          Eigen::VectorXd state(6);
+          state << x_delay, 0, 0, v, cte, epsi;     // Vehicle coordinates: px, py, psi is zero
+
+
+          // Pass state and coefficients to MPC
+          auto vars = mpc.Solve(state, coeffs);
+
+          // Length front to CoG
+          const double Lf = 2.67;
+
+
+
+
           double steer_value;
           double throttle_value;
+
+          steer_value = -vars[0] / (deg2rad(25.0) * Lf);
+          throttle_value = vars[1];
 
           json msgJson;
           // NOTE: Remember to divide by deg2rad(25) before you send the steering value back.
@@ -107,9 +154,18 @@ int main() {
           msgJson["steering_angle"] = steer_value;
           msgJson["throttle"] = throttle_value;
 
-          //Display the MPC predicted trajectory 
+          //Display the MPC predicted trajectory
           vector<double> mpc_x_vals;
           vector<double> mpc_y_vals;
+
+          for (int i=2; i<vars.size(); i++) {
+            if (i%2 == 0) {
+              mpc_x_vals.push_back(vars[i]);
+            }
+            else {
+              mpc_y_vals.push_back(vars[i]);
+            }
+          }
 
           //.. add (x,y) points to list here, points are in reference to the vehicle's coordinate system
           // the points in the simulator are connected by a Green line
@@ -127,6 +183,12 @@ int main() {
           msgJson["next_x"] = next_x_vals;
           msgJson["next_y"] = next_y_vals;
 
+          double poly_inc = 2.5;
+          int num_points = 25;
+          for (int i=1; i<num_points; i++) {
+            next_x_vals.push_back(poly_inc * i);
+            next_y_vals.push_back(polyeval(coeffs, poly_inc * i));
+          }
 
           auto msg = "42[\"steer\"," + msgJson.dump() + "]";
           std::cout << msg << std::endl;
